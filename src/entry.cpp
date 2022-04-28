65ac18e238083205e9c746d9ec747e7040fc8abc
Samuel Wiseman <samw@gov.com>
2022-04-27 21:45:40 -0500

    file input checks

--- src/entry.cpp
+++ src/entry.cpp
@@ -6,24 +6,29 @@
 
 using Debug = Common::Debug;
 Debug Dbg;
-/*
-int main(int argc, char **argv)  {
-    // make sure ghc::filesystem works
-    std::cout << "current path: " << ghc::filesystem::current_path() << std::endl;
 
-    Common::PrintSystemInformation();
+int main(int argc, char **argv)  {
+
     if(strcmp(Common::GetOperatingSystemName(), "linux")) {
     Dbg.LogData(Debug::LogType::Error, "Unsupported Operating System");
     return 0;
     }
-    args::ArgumentParser parser("MultiChem by @notSam25", "Thanks for building!");
+
+    Common::PrintSystemInformation();
+    args::ArgumentParser parser("MultiChem by @notSam25", "Licence info on github.com/notSam25/MultiChem");
     args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
     args::Group commands(parser, "Commands");
-    args::ValueFlag<std::string> message(parser, "MESSAGE", "commit message", {'m'});
+    args::ValueFlag<std::string> message(parser, "PATH", "Path to JSON input", {'p'});
+    
     try {
         parser.ParseCLI(argc, argv);
         if(message) {
             std::cout << "Path: " << args::get(message) << std::endl;
+            if(ghc::filesystem::exists(message->c_str()) && strcmp(ghc::filesystem::path(message->c_str()).extension().c_str(), ".json")) {
+                // File exists and has the extension of .json
+
+            } else
+                Dbg.LogData(Debug::LogType::Error, "File does not exist!");
         }
     }
     catch (const args::Completion& e) {
@@ -39,9 +44,5 @@ int main(int argc, char **argv)  {
         std::cerr << parser;
         return -1;
     }
-    return 0;
-}*/
-
-int main()  {
     return 0;
 }
