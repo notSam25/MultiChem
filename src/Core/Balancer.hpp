35dc93399bffc1a1b805beaef74cbf16310e6b7a
Samuel Wiseman <samw@gov.com>
2022-04-28 20:40:44 -0500

    add reaction statments

--- src/Core/Balancer.hpp
+++ src/Core/Balancer.hpp
@@ -12,5 +12,18 @@ void BalanceEquation(nlohmann::json Balance) {
 
   if (Balance.value("ReactionType", "") == "")
     std::runtime_error("BalanceEquation.ReactionType error");
+    
+  // Reaction types: synthesis. decomposition, combustion, double-replacement,
+  // single-replacement
+  if (Balance.value("ReactionType", "") == "synthesis") {
+  }
+  if (Balance.value("ReactionType", "") == "decomposition") {
+  }
+  if (Balance.value("ReactionType", "") == "combustion") {
+  }
+  if (Balance.value("ReactionType", "") == "double-replacement") {
+  }
+  if (Balance.value("ReactionType", "") == "single-replacement") {
+  }
 }
 } // namespace mc::BalanceEquation
