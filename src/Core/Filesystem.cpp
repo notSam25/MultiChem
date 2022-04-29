52b6cf1761bdfc8bb1cd9c0e65ca7d8b27ccfb5b
Abheek Dhawan <abheekd@protonmail.com>
2022-04-29 16:53:10 +0000

    build(cmake): allow compilation with filesystem for older compiler versions
    The std::filesystem library needs special linkage and headers before C++17

--- src/Core/Filesystem.cpp
+++ src/Core/Filesystem.cpp
@@ -1,4 +1,12 @@
+#include <version> // to check for filesystem support (provides macros)
+
+#ifdef __cpp_lib_filesystem
 #include <filesystem>
+#elif __cpp_lib_experimental_filesystem
+#include <experimental/filesystem>
+#else
+#error "could not find any sort of filesystem support"
+#endif
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -6,7 +14,11 @@
 
 #include "Core/Filesystem.hpp"
 
+#ifdef __cpp_lib_filesystem
 namespace fs = std::filesystem;
+#elif __cpp_lib_experimental_filesystem
+namespace fs = std::experimental::filesystem;
+#endif
 
 namespace mc::Filesystem {
 std::string GetFileContents(const char *name) {
